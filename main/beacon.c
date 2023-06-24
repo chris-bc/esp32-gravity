12b78ca4a18a7a82cf40014317a7fb42c58de67a
Chris <chris@bennettscash.id.au>
2023-06-24 17:57:10 +1000

    Now working

--- main/beacon.c
+++ main/beacon.c
@@ -1,6 +1,16 @@
 #include "beacon.h"
 
+/*
+ * This is the (currently unofficial) 802.11 raw frame TX API,
+ * defined in esp32-wifi-lib's libnet80211.a/ieee80211_output.o
+ *
+ * This declaration is all you need for using esp_wifi_80211_tx in your own application.
+ */
+esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
+
 static char **attack_ssids = NULL;
+static char **user_ssids = NULL;
+static int user_ssid_count = 0;
 
 #define BEACON_SSID_OFFSET 38
 #define SRCADDR_OFFSET 10
@@ -11,6 +21,31 @@ static beacon_attack_t attackType;
 static int SSID_COUNT = DEFAULT_SSID_COUNT;
 
 static TaskHandle_t task;
+
+static uint8_t beacon_raw[] = {
+	0x80, 0x00,							    // 0-1: Frame Control
+	0x00, 0x00,						    	// 2-3: Duration
+	0xff, 0xff, 0xff, 0xff, 0xff, 0xff,				// 4-9: Destination address (broadcast)
+	0xba, 0xde, 0xaf, 0xfe, 0x00, 0x06,		// 10-15: Source address
+	0xba, 0xde, 0xaf, 0xfe, 0x00, 0x06,		// 16-21: BSSID
+	0x00, 0x00,				    		// 22-23: Sequence / fragment number
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,     // 24-31: Timestamp (GETS OVERWRITTEN TO 0 BY HARDWARE)
+	0x64, 0x00,			    			// 32-33: Beacon interval
+	0x31, 0x04,		    				// 34-35: Capability info
+	0x00, 0x00, /* FILL CONTENT HERE */	// 36-38: SSID parameter set, 0x00:length:content
+	0x01, 0x08, 0x82, 0x84,	0x8b, 0x96, 0x0c, 0x12, 0x18, 0x24,	// 39-48: Supported rates
+	0x03, 0x01, 0x01,				// 49-51: DS Parameter set, current channel 1 (= 0x01),
+	0x05, 0x04, 0x01, 0x02, 0x00, 0x00,		// 52-57: Traffic Indication Map
+	
+};
+
+int countSsid() {
+	return user_ssid_count;
+}
+
+char **lsSsid() {
+	return user_ssids;
+}
 
 int addSsid(char *ssid) {
 	printf("Commencing addSsid(\"%s\"). target-ssids contains %d values:\n", ssid, user_ssid_count);
--- main/beacon.h
+++ main/beacon.h
@@ -12,7 +12,6 @@
 
 // Allowable chars for randomly-generated SSIDs
 static char ssid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-static char **user_ssids;
 static char *rick_ssids[] = {
 	"01 Never gonna give you up",
 	"02 Never gonna let you down",
@@ -23,6 +22,7 @@ static char *rick_ssids[] = {
 	"07 Never gonna tell a lie",
 	"08 and hurt you"
 };
+#define RICK_SSID_COUNT 8
 
 typedef enum {
     ATTACK_BEACON_NONE,
@@ -31,34 +31,7 @@ typedef enum {
     ATTACK_BEACON_USER
 } beacon_attack_t;
 
-/*
- * This is the (currently unofficial) 802.11 raw frame TX API,
- * defined in esp32-wifi-lib's libnet80211.a/ieee80211_output.o
- *
- * This declaration is all you need for using esp_wifi_80211_tx in your own application.
- */
-esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
-
-static uint8_t beacon_raw[] = {
-	0x80, 0x00,							    // 0-1: Frame Control
-	0x00, 0x00,						    	// 2-3: Duration
-	0xff, 0xff, 0xff, 0xff, 0xff, 0xff,				// 4-9: Destination address (broadcast)
-	0xba, 0xde, 0xaf, 0xfe, 0x00, 0x06,		// 10-15: Source address
-	0xba, 0xde, 0xaf, 0xfe, 0x00, 0x06,		// 16-21: BSSID
-	0x00, 0x00,				    		// 22-23: Sequence / fragment number
-	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,     // 24-31: Timestamp (GETS OVERWRITTEN TO 0 BY HARDWARE)
-	0x64, 0x00,			    			// 32-33: Beacon interval
-	0x31, 0x04,		    				// 34-35: Capability info
-	0x00, 0x00, /* FILL CONTENT HERE */	// 36-38: SSID parameter set, 0x00:length:content
-	0x01, 0x08, 0x82, 0x84,	0x8b, 0x96, 0x0c, 0x12, 0x18, 0x24,	// 39-48: Supported rates
-	0x03, 0x01, 0x01,				// 49-51: DS Parameter set, current channel 1 (= 0x01),
-	0x05, 0x04, 0x01, 0x02, 0x00, 0x00,		// 52-57: Traffic Indication Map
-	
-};
-
 #define DEFAULT_SSID_COUNT 20
-#define RICK_SSID_COUNT 8
-static int user_ssid_count = 0;
 static int SSID_LEN_MIN = 8;
 static int SSID_LEN_MAX = 32;
 
@@ -66,3 +39,5 @@ int beacon_start(beacon_attack_t type, int ssidCount);
 int beacon_stop();
 int addSsid(char *ssid);
 int rmSsid(char *ssid);
+int countSsid();
+char **lsSsid();
