f79746c28161fab46e4c1ad3619d162b71f9d8bf
Chris <chris@bennettscash.id.au>
2023-07-04 00:18:13 +1000

    scaffolding in place for deauth now - starting the event loop

--- main/deauth.c
+++ main/deauth.c
@@ -23,17 +23,35 @@ static bool spoofMAC = DEAUTH_MAC_DEFAULT;
 static long delay = DEAUTH_MILLIS_DEFAULT;
 static TaskHandle_t deauthTask = NULL;
 
+void deauthLoop(void *pvParameter) {
+    //
+    while (true) {
+        vTaskDelay(100 / portTICK_PERIOD_MS); /* Delay 100ms */
+    }
+}
+
 esp_err_t deauth_start(DeauthMode dMode, bool setMAC, long millis) {
     if (mode != DEAUTH_MODE_OFF) {
         /* Deauth is already running, stop that one */
-
+        deauth_stop();
     }
-
+    mode = dMode;
+    spoofMAC = setMAC;
+    delay = millis;
+    if (mode == DEAUTH_MODE_OFF) {
+        return ESP_OK;
+    }
+    xTaskCreate(&deauthLoop, "deauthLoop", 2048, NULL, 5, &deauthTask);
     return ESP_OK;
 }
 
 esp_err_t deauth_stop() {
-    // TODO: Clean up
+    // TODO: COther things
+    if (deauthTask != NULL) {
+        vTaskDelete(deauthTask);
+        deauthTask = NULL;
+        mode = DEAUTH_MODE_OFF;
+    }
 
     return ESP_OK;
 }
