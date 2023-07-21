8192eb157ca2d4dcc2910c6cb5a5bf1824e0d387
Chris Bennetts-Cash <chris@bennettscash.id.au>
2023-07-21 17:43:48 +1000

    Starting bluetooth from scratch. So far so good

---
+++ main/bluetooth.c
@@ -0,0 +1,164 @@
+#include "bluetooth.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_err.h"
+#include "esp_gap_bt_api.h"
+#include <stddef.h>
+
+const char *BT_TAG = "bt@GRAVITY";
+
+static char *bda2str(esp_bd_addr_t bda, char *str, size_t size) {
+    if (bda == NULL || str == NULL || size < 18) {
+        return NULL;
+    }
+
+    uint8_t *p = bda;
+    sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x", p[0], p[1], p[2], p[3], p[4], p[5]);
+    return str;
+}
+
+static char *uuid2str(esp_bt_uuid_t *uuid, char *str, size_t size) {
+    if (uuid == NULL || str == NULL) {
+        return NULL;
+    }
+
+    if (uuid->len == 2 && size >= 5) {
+        sprintf(str, "%04x", uuid->uuid.uuid16);
+    } else if (uuid->len == 4 && size >= 9) {
+        sprintf(str, "%08"PRIx32, uuid->uuid.uuid32);
+    } else if (uuid->len == 16 && size >= 37) {
+        uint8_t *p = uuid->uuid.uuid128;
+        sprintf(str, "%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+                p[15], p[14], p[13], p[12], p[11], p[10], p[9], p[8], p[7], p[6], p[5], p[4],
+                p[3], p[2], p[1], p[0]);
+    } else {
+        return NULL;
+    }
+    return str;
+}
+
+static bool get_name_from_eir(uint8_t *eir, uint8_t *bdname, uint8_t *bdname_len)
+{
+    uint8_t *rmt_bdname = NULL;
+    uint8_t rmt_bdname_len = 0;
+
+    if (!eir) {
+        return false;
+    }
+
+    rmt_bdname = esp_bt_gap_resolve_eir_data(eir, ESP_BT_EIR_TYPE_CMPL_LOCAL_NAME, &rmt_bdname_len);
+    if (!rmt_bdname) {
+        rmt_bdname = esp_bt_gap_resolve_eir_data(eir, ESP_BT_EIR_TYPE_SHORT_LOCAL_NAME, &rmt_bdname_len);
+    }
+
+    if (rmt_bdname) {
+        if (rmt_bdname_len > ESP_BT_GAP_MAX_BDNAME_LEN) {
+            rmt_bdname_len = ESP_BT_GAP_MAX_BDNAME_LEN;
+        }
+
+        if (bdname) {
+            memcpy(bdname, rmt_bdname, rmt_bdname_len);
+            bdname[rmt_bdname_len] = '\0';
+        }
+        if (bdname_len) {
+            *bdname_len = rmt_bdname_len;
+        }
+        return true;
+    }
+
+    return false;
+}
+
+void update_device_info(esp_bt_gap_cb_param_t *param) {
+    //
+}
+
+void bt_gap_init() {
+    app_gap_cb_t *p_dev = &m_dev_info;
+    memset(p_dev, 0, sizeof(app_gap_cb_t));
+
+    p_dev->state = APP_GAP_STATE_IDLE;
+}
+
+static void bt_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param) {
+    app_gap_cb_t *p_dev = &m_dev_info;
+    char bda_str[18];
+    char uuid_str[37];
+
+    switch (event) {
+        case ESP_BT_GAP_DISC_RES_EVT:
+            update_device_info(param);
+            break;
+        case ESP_BT_GAP_DISC_STATE_CHANGED_EVT:
+            if (param->disc_st_chg.state == ESP_BT_GAP_DISCOVERY_STOPPED) {
+                ESP_LOGI(BT_TAG, "Device discovery stopped.");
+                if ( (p_dev->state == APP_GAP_STATE_DEVICE_DISCOVER_COMPLETE ||
+                        p_dev->state == APP_GAP_STATE_DEVICE_DISCOVERING) && p_dev->dev_found) {
+                    p_dev->state = APP_GAP_STATE_SERVICE_DISCOVERING;
+                    ESP_LOGI(BT_TAG, "Discover services...");
+                    esp_bt_gap_get_remote_services(p_dev->bda);
+                }
+            } else if (param->disc_st_chg.state == ESP_BT_GAP_DISCOVERY_STARTED) {
+                ESP_LOGI(BT_TAG, "Discovery started");
+            }
+            break;
+        case ESP_BT_GAP_RMT_SRVCS_EVT:
+            if (memcmp(param->rmt_srvcs.bda, p_dev->bda, ESP_BD_ADDR_LEN) == 0 &&
+                    p_dev->state == APP_GAP_STATE_SERVICE_DISCOVERING) {
+                p_dev->state = APP_GAP_STATE_SERVICE_DISCOVER_COMPLETE;
+                if (param->rmt_srvcs.stat == ESP_BT_STATUS_SUCCESS) {
+                    ESP_LOGI(BT_TAG, "Services for device %s found", bda2str(p_dev->bda, bda_str, 18));
+                    for (int i = 0; i < param->rmt_srvcs.num_uuids; ++i) {
+                        esp_bt_uuid_t *u = param->rmt_srvcs.uuid_list + i;
+                        ESP_LOGI(BT_TAG, "--%s", "TODO");
+                    }
+                } else {
+                    ESP_LOGI(BT_TAG, "Services for device %s not found", bda2str(p_dev->bda, bda_str, 18));
+                }
+            }
+            break;
+        case ESP_BT_GAP_RMT_SRVC_REC_EVT:
+        default:
+            ESP_LOGI(BT_TAG, "event: %d", event);
+            break;
+    }
+    return;
+}
+
+void bt_gap_start() {
+    esp_bt_gap_register_callback(bt_gap_cb);
+    /* Set Device name */
+    char *dev_name = "GRAVITY_INQUIRY";
+    esp_bt_dev_set_device_name(dev_name);
+
+    /* Set discoverable and connectable, wait to be connected */
+    esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+
+    bt_gap_init();
+
+    /* Start to discover nearby devices */
+    app_gap_cb_t *p_dev = &m_dev_info;
+    p_dev->state = APP_GAP_STATE_DEVICE_DISCOVERING;
+    esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_GENERAL_INQUIRY, 10, 0);
+}
+
+void testBT() {
+    esp_err_t err = ESP_OK; /* return value */
+    err = esp_bt_controller_mem_release(ESP_BT_MODE_BLE);
+    ESP_LOGI(BT_TAG, "Controller mem release returned %s", esp_err_to_name(err));
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    err = esp_bt_controller_init(&bt_cfg);
+    ESP_LOGI(BT_TAG, "BT controller init returned %s", esp_err_to_name(err));
+
+    err = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT);
+    ESP_LOGI(BT_TAG, "BT Controller enable returned %s", esp_err_to_name(err));
+
+    err = esp_bluedroid_init();
+    ESP_LOGI(BT_TAG, "BlueDroid init returned %s", esp_err_to_name(err));
+
+    err = esp_bluedroid_enable();
+    ESP_LOGI(BT_TAG, "BlueDroid enable returned %s", esp_err_to_name(err));
+
+    bt_gap_start();
+}
---
+++ main/bluetooth.h
@@ -0,0 +1,34 @@
+#ifndef BLUETOOTH_H
+#define BLUETOOTH_H
+
+#include <esp_gap_bt_api.h>
+#include <esp_bt.h>
+#include <esp_bt_main.h>
+#include <esp_bt_device.h>
+#include <esp_log.h>
+
+typedef enum {
+    APP_GAP_STATE_IDLE = 0,
+    APP_GAP_STATE_DEVICE_DISCOVERING,
+    APP_GAP_STATE_DEVICE_DISCOVER_COMPLETE,
+    APP_GAP_STATE_SERVICE_DISCOVERING,
+    APP_GAP_STATE_SERVICE_DISCOVER_COMPLETE,
+} app_gap_state_t;
+
+typedef struct {
+    bool dev_found;
+    uint8_t bdname_len;
+    uint8_t eir_len;
+    uint8_t rssi;
+    uint32_t cod;
+    uint8_t eir[ESP_BT_GAP_EIR_DATA_LEN];
+    uint8_t bdname[ESP_BT_GAP_MAX_BDNAME_LEN + 1];
+    esp_bd_addr_t bda;
+    app_gap_state_t state;
+} app_gap_cb_t;
+
+static app_gap_cb_t m_dev_info;
+extern const char *BT_TAG;
+
+
+#endif
